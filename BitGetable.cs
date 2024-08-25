4e82be4ff1d88185e1acefbaff3b1f84828223fe
ModerrOn <sensansion@gmail.com>
2024-08-26 01:28:40 +0500

    Окончательное

---
+++ BitGetable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hometask_sem_2;
+
+namespace Hometask_sem_2
+{ };
+class BitGetable
+{
+}
+
+public interface IBitGetable
+{
+    bool GetBitByIndex(byte index);
+    void SetBitByIndex(byte index, bool value);
+}
+
+public class Bits : IBitGetable  // internal class Bits : IBitGetable
+{
+    public long Value { get; private set; } //поменяли с: public byte Value { get; private set; }
+                                            // свойство для неявного преобразования
+    public byte ValueByte { get; private set; }
+    public int ValueInt { get; private set; }
+    private int Size { get; set; } //private
+    public Bits(byte value)
+    {
+        Value = value;
+        Size = sizeof(byte);
+    }
+    public Bits(int value)
+    {
+        Value = value;
+        Size = sizeof(int);
+    }
+    public Bits(long value)
+    {
+        Value = value;
+        Size = sizeof(long);
+    }
+
+    public bool GetBitByIndex(byte index) //     public bool GetBitByIndex(byte index)
+    {
+        return (Value & (1 << index)) != 0;
+    }
+    public void SetBitByIndex(byte index, bool value) //    public void SetBitByIndex(byte index, bool value)
+    {
+        if (value)
+        {
+            Value |= (byte)(1 << index);
+        }
+        else
+        {
+            Value &= (byte)~(1 << index);
+        }
+    }
+    public bool this[byte index] //переопределение
+    {
+        get => GetBitByIndex(index);
+        set => SetBitByIndex(index, value);
+    }
+    public static implicit operator long(Bits bits) => bits.Value; //неявное приведение переопределение было byte(Bits bits) => bits.Value;
+    public static explicit operator Bits(long value) => new(value);//явное приведение переопределение Bits(byte value) => new(value)
+    public static explicit operator Bits(byte value) => new(value);
+    public static explicit operator Bits(int value) => new(value);
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Hometask_sem_2;
+
+namespace Hometask_sem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine("Неявное приведение");
+            Bits bit = new(80);
+            Bits bitByte = new((byte)60);
+            Bits bitInt = new((int)70);
+            Bits bitLong = new((long)80);
+
+            Console.WriteLine(bit);
+            Console.WriteLine(bitByte);
+            Console.WriteLine(bitInt);
+            Console.WriteLine(bitLong);
+            Console.WriteLine("----------");
+
+
+            Console.WriteLine("Явное приведение");
+            bit = (Bits)257;
+            byte bByte = 255;
+            int cInt = 2147483647;
+            long dlong = 9223372036854775807;
+
+            Console.WriteLine(bit.Value);
+            bit = (Bits)bByte;
+            Console.WriteLine(bit.Value);
+            bit = (Bits)cInt;
+            Console.WriteLine(bit.Value);
+            bit = (Bits)dlong;
+            Console.WriteLine(bit.Value);
+
+
+        }
+    }
+}
